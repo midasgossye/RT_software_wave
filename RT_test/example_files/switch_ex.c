ee2c5ff9fd6c07fbcf48a616ba9ff02504777049
midasgossye <midasgossye@hotmail.com>
2017-11-13 16:18:17 +0800

    added QNX latest folder

---
+++ RT_test/example_files/switch_ex.c
@@ -0,0 +1,168 @@
+// 09September 2005
+//******************************************************************************************************
+// Performs basic I/O for the Omega PCI-DAS1602 
+//
+// Demonstration routine to demonstrate pci hardware programming
+// Demonstrated the most basic DIO and ADC and DAC functions
+// - excludes FIFO and strobed operations 
+//
+// Note :
+//
+//			22 Sept 2016 : Restructured to demonstrate Sine wave to DA
+//
+// G.Seet - 26 August 2005
+//******************************************************************************************************
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <hw/pci.h>
+#include <hw/inout.h>
+#include <sys/neutrino.h>
+#include <sys/mman.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
+																
+#define	INTERRUPT		iobase[1] + 0				// Badr1 + 0 : also ADC register
+#define	MUXCHAN			iobase[1] + 2				// Badr1 + 2
+#define	TRIGGER			iobase[1] + 4				// Badr1 + 4
+#define	AUTOCAL			iobase[1] + 6				// Badr1 + 6
+#define 	DA_CTLREG		iobase[1] + 8				// Badr1 + 8
+
+#define	 AD_DATA			iobase[2] + 0				// Badr2 + 0
+#define	 AD_FIFOCLR		iobase[2] + 2				// Badr2 + 2
+
+#define	TIMER0				iobase[3] + 0				// Badr3 + 0
+#define	TIMER1				iobase[3] + 1				// Badr3 + 1
+#define	TIMER2				iobase[3] + 2				// Badr3 + 2
+#define	COUNTCTL			iobase[3] + 3				// Badr3 + 3
+#define	DIO_PORTA		iobase[3] + 4				// Badr3 + 4
+#define	DIO_PORTB		iobase[3] + 5				// Badr3 + 5
+#define	DIO_PORTC		iobase[3] + 6				// Badr3 + 6
+#define	DIO_CTLREG		iobase[3] + 7				// Badr3 + 7
+#define	PACER1				iobase[3] + 8				// Badr3 + 8
+#define	PACER2				iobase[3] + 9				// Badr3 + 9
+#define	PACER3				iobase[3] + a				// Badr3 + a
+#define	PACERCTL			iobase[3] + b				// Badr3 + b
+
+#define 	DA_Data			iobase[4] + 0				// Badr4 + 0
+#define	DA_FIFOCLR		iobase[4] + 2				// Badr4 + 2
+	
+int badr[5];															// PCI 2.2 assigns 6 IO base addresses
+float frequency_gen = 1.0	;
+int sleep_for;
+float amplitude_value = 2.5;
+float mean_value = 2.5;
+unsigned short chan;
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+int main() {
+bool running = true;
+int leds = 0x0;
+int resolution = 50;
+int msec;
+clock_t start, diff;
+struct pci_dev_info info;
+void *hdl;
+
+uintptr_t iobase[6];
+uintptr_t dio_in;
+uint16_t adc_in;
+int output_x;
+
+
+int i;
+int j;
+unsigned int count;
+unsigned short chan;
+
+unsigned int data[100];
+float delta,dummy;
+
+printf("\fDemonstration Routine for PCI-DAS 1602\n\n");
+
+memset(&info,0,sizeof(info));
+if(pci_attach(0)<0) {
+  perror("pci_attach");
+  exit(EXIT_FAILURE);
+  }
+
+																		/* Vendor and Device ID */
+info.VendorId=0x1307;
+info.DeviceId=0x01;
+
+if ((hdl=pci_attach_device(0, PCI_SHARE|PCI_INIT_ALL, 0, &info))==0) {
+  perror("pci_attach_device");
+  exit(EXIT_FAILURE);
+  }
+  																		// Determine assigned BADRn IO addresses for PCI-DAS1602			
+
+printf("\nDAS 1602 Base addresses:\n\n");
+for(i=0;i<5;i++) {
+  badr[i]=PCI_IO_ADDR(info.CpuBaseAddress[i]);
+  printf("Badr[%d] : %x\n", i, badr[i]);
+  }
+ 
+printf("\nReconfirm Iobase:\n");  						// map I/O base address to user space						
+for(i=0;i<5;i++) {												// expect CpuBaseAddress to be the same as iobase for PC
+  iobase[i]=mmap_device_io(0x0f,badr[i]);	
+  printf("Index %d : Address : %x ", i,badr[i]);
+  printf("IOBASE  : %x \n",iobase[i]);
+  }													
+																		// Modify thread control privity
+if(ThreadCtl(_NTO_TCTL_IO,0)==-1) {
+  perror("Thread Control");
+  exit(1);
+  }																											
+   													    //******************************************************************************
+	
+  														// Unreachable code
+	out8(DIO_CTLREG,0x10);		//Sets PORTA as INPUT and PORTB and C as OUTPUTS								
+	leds = 0x1;																		
+	dio_in=in8(DIO_PORTC); printf("Port C : %02x\n",dio_in);	// Read back		
+	
+//******************************************************************************
+// ADC Port Functions
+//******************************************************************************
+							// Initialise Board								
+out16(INTERRUPT,0x60c0);				// sets interrupts	 - Clears			
+out16(TRIGGER,0x2081);// sets trigger control: 10MHz, clear, Burst off,SW trig. default:20a0
+out16(AUTOCAL,0x007f);		// sets automatic calibration : default
+
+out16(AD_FIFOCLR,0); 		// clear ADC buffer
+out16(MUXCHAN,0x0900);		// Write to MUX register-SW trigger,UP,DE,5v,ch 0-0 	
+
+					// x x 0 0 | 1  0  0 1  | 0x 7   0 | Diff - 8 channels
+					// SW trig |Diff-Uni 5v| scan 0-7| Single - 16 channels	
+	count = 0;
+	chan= ((count & 0x0f)<<4) | (0x0f & count);
+	out16(MUXCHAN,0x0900|chan);
+
+	while(running) {
+	 		// Set channel	 - burst mode off.
+  delay(1);					// allow mux to settle
+  out16(AD_DATA,0); 				// start ADC 
+  while(!(in16(MUXCHAN) & 0x4000));
+  adc_in=in16(AD_DATA);   
+  printf("ADC Chan: %02x Data [%3d]: %4x \n", chan, (int)count, (unsigned int)adc_in);							// print ADC													
+  fflush( stdout );
+  delay(5);			// Write to MUX register - SW trigger, UP, DE, 5v, ch 0-7 	
+		dio_in=in8(DIO_PORTA); printf("Port A : %02x\n",dio_in);	// Read back
+		if(dio_in > 0xf0) running = false;
+		
+		//if(adc_in < 0x1000) output_x = 0x1;
+		//else if(adc_in < 0x2000) output_x = 0x2;
+		//else if(adc_in < 0x3000) output_x = 0x4;
+		//else if(adc_in > 0x3000) output_x= 0x8;
+		printf("%d \n",adc_in);
+		delay(5);																
+		out8(DIO_PORTB,leds);						// Write to Port B  		
+		
+		
+		leds*=2;
+		if(leds > 0xf) leds = 0x1;
+
+	
+	}
+}
---
+++ RT_test/menu.c
@@ -0,0 +1,447 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ncurses.h>
+#include <pthread.h>
+
+#define FREQ_LOWER_BOUND 0.1
+#define FREQ_UPPER_BOUND 10
+#define AMPL_LOWER_BOUND 0
+#define AMPL_UPPER_BOUND 5
+#define MEAN_LOWER_BOUND 0
+#define MEAN_UPPER_BOUND 5
+
+#ifdef _WIN32
+#define CLEARSTR "cls"
+#define DIRLIST "dir config_files /b /a-d"
+#else
+#define CLEARSTR "clear"
+#define DIRLIST "ls -1 config_files"
+#endif
+
+// === GLOBAL VARIABLES ===
+static double ampl_val = 2.5;
+static double mean_val = 2.5;
+static double freq_val = 10.0;
+static int out_ADC = 0;
+//"cls" for win/DOS
+//"clear" for Unix
+//char *CLEARSTR = "cls";
+
+
+void flush_stdin(void) {
+	int c;
+	while((c = getchar()) != '\n' && c != EOF);
+}
+
+void remove_space(char *d, const char *s){
+    for(;*s;++s){
+        if(*s != ' ')
+            *d++ = *s;
+    }
+    *d = *s;
+}
+
+char switch_check(void) {
+	return 0;
+}
+
+int choice_checker(int lower_bound, int upper_bound) {
+    char valid_in = 1;
+    int choice;
+    //flush_stdin();
+    do {
+        printf("\nChoice: ");
+        fflush(stdout);
+       do {
+       	choice = getch();
+       }  while(choice==ERR && switch_check() == 0);
+        choice = choice - '0';
+        if(choice < lower_bound || choice > upper_bound) {
+            printf("Invalid choice!\n");
+            valid_in = 0;
+        }
+        else valid_in = 1;
+
+
+    } while(!valid_in);
+    //printf("%d",choice);
+    flush_stdin();
+    return choice;
+
+}
+char main_menu(void) {
+    system(CLEARSTR);
+
+    printf("\t\t** Waveform generator V1.0 **\n");
+    printf("\t\t=============================\n\n");
+    printf("1)\tConfigure DAC\n");
+    printf("2)\tGenerate Square Wave\n");
+    printf("3)\tGenerate Sine Wave\n");
+    printf("4)\tGenerate Triangle Wave\n");
+    printf("5)\tLoad config file\n");
+    printf("6)\tHelp\n\n");
+    printf("7)\tQuit\n");
+    return choice_checker(1,7);
+
+}
+
+void load_help_menu(void) {
+	char dummy;
+	system(CLEARSTR);
+
+	printf("\t\t** HELP MENU **\n");
+	printf("\t\t===============\n\n");
+	printf("\tPROGRAM DESCRIPTION\n");
+	printf("This program uses the PCI-DAS1602/16 ADC/DAC Card to generate sine, square and triangle signals.\n");
+	printf("This program also serves as the second Continuous Assesment for the NTU R/T software course \nto prove the authors proficiency in the C programming lamguage and QNX OS.\n\n");
+
+	printf("\tBASIC PROGRAM USAGE\n");
+	printf("The user can setup the DAC paramters by setting them in the DAC configuration menu.\n");
+	printf("Care must be taken to make sure the signal will not exceed the 0-5 V boundary, otherwise the signal\nwill clip to the supply rail(s).\n");
+	printf("Apart from setting the signals amplitude, mean value and frequency, the user can also select which \nDAC channel he would like to use.\n\n");
+
+	printf("\tSAVING AND LOADING CONFIG FILES\n");
+	printf("If the user has set his desired DAC configuration parameters, he can choose to save this configuration\nto a config file. This is very helpful if the user needs the same paramters at another time.\n");
+	printf("You can save the current configuration by choosing the 'save configuration in config file' option in the \nDAC configuration menu. To save a file, type in a file name followed by the .txt extension (e.g. config1.txt)\n");
+	printf("To load a previously saved config file, select the 'Load config file' option in the main menu. A list of all\nthe available config files will be shown. To choose a config file, \ntype in the name of the file as shown in the listing (including the file extension).\n");
+	printf("When the file is loaded in succesfully, all of the loaded parameters will be shown. If an error occured, \ntry again and double check if you spelled the desired file correctly and included the file extension.\n\n");
+	printf("Press enter to go back to the main menu...");
+	fflush(stdout);
+	dummy = getchar();
+
+}
+
+double config_amplitude(void) {
+   double ampl_value;
+   char valid_in = 1;
+   float lower_bound = AMPL_LOWER_BOUND;
+   float upper_bound = AMPL_UPPER_BOUND;
+
+   system(CLEARSTR);
+
+
+
+    do {
+        printf("Amplitude value (%.1f-%.1f V): ",lower_bound, upper_bound );
+        fflush(stdout);
+        ampl_value = 0;
+        if(scanf("%lf", &ampl_value)!=1) {
+           printf("\nInvalid Input!\n");
+        }
+        getchar();
+        if(ampl_value <= AMPL_LOWER_BOUND || ampl_value > AMPL_UPPER_BOUND) {
+            printf("\nValue out of range!\n");
+            valid_in = 0;
+        }
+        else valid_in = 1;
+    } while(!valid_in);
+    //printf("%lf",ampl_value);
+    return ampl_value;
+}
+
+double config_mean(void) {
+    double mean_value;
+    char valid_in = 1;
+    float lower_bound = MEAN_LOWER_BOUND;
+    float upper_bound = MEAN_UPPER_BOUND;
+    system(CLEARSTR);
+
+    do {
+        printf("Mean Value (%.0f-%.0f V): ", lower_bound, upper_bound);
+        fflush(stdout);
+        if(scanf("%lf", &mean_value)!=1) {
+           printf("\nInvalid Input!\n");
+        }
+        getchar();
+        if(mean_value <= MEAN_LOWER_BOUND || mean_value > MEAN_UPPER_BOUND) {
+            printf("\nValue out of range!\n");
+            valid_in = 0;
+        }
+        else valid_in = 1;
+    } while(!valid_in);
+    //printf("%lf",mean_val);
+    return mean_value;
+}
+
+double config_freq(void) {
+    double freq_value;
+    char valid_in = 1;
+    float lower_bound = FREQ_LOWER_BOUND;
+    float upper_bound = FREQ_UPPER_BOUND;
+    system(CLEARSTR);
+
+    do {
+        printf("Frequency (%.1f-%.1f Hz): ", lower_bound, upper_bound);
+        fflush(stdout);
+        if(scanf("%lf", &freq_value)!=1) {
+           printf("\nInvalid Input!\n");
+        }
+        getchar();
+        if(freq_value < FREQ_LOWER_BOUND || freq_value > FREQ_UPPER_BOUND) {
+            printf("\nValue out of range!\n");
+            valid_in = 0;
+        }
+        else valid_in = 1;
+    } while(!valid_in);
+    //printf("%lf",freq_value);
+    return freq_value;
+}
+
+char check_mean_amp_val(double amp_value, double mean_value) {
+    char compatible = 1;
+    if(amp_value + mean_value > 5) {
+        compatible = 0;
+    }
+    else if(mean_value - amp_value < 0) {
+        compatible = 0;
+    }
+
+    if(!compatible) {
+        printf("\nWarning: Waveform will clip with current waveform settings.\n         Please change the amplitude and/or the mean value of the signal before exiting this menu.\n");
+    }
+    return compatible;
+
+}
+
+void generate_config_file(void) {
+    char file_name_buff[60];
+    char *path_name = "config_files/";
+    char * file_path;
+    FILE *fp;
+    char dummy;
+    char *file_name;
+
+
+    system(CLEARSTR);
+    printf("\t\t** Configuration file creator **\n");
+    printf("\t\t================================\n\n");
+    printf("Config file name: ");
+    fflush(stdout);
+
+    fgets (file_name_buff, 60, stdin);
+    file_name = malloc(strlen(file_name_buff));
+    remove_space(file_name, file_name_buff);
+    strtok(file_name, "\n");
+
+    file_path = malloc(strlen(path_name)+strlen(file_name)+1);
+    file_path[0] = '\0';
+    strcat(file_path, path_name);
+    strcat(file_path, file_name);
+
+    //printf("%s, %d", file_path, strlen(file_path));
+
+    fp = fopen(file_path, "w+");
+    fprintf(fp, "AMPL=%lf MEAN=%lf FREQ=%lf ADC=%d",ampl_val, mean_val, freq_val, out_ADC);
+    fclose(fp);
+    printf("Config file created with filename %s in folder %s\nPress enter to continue...",file_name, path_name);
+    fflush(stdout);
+    dummy = getchar();
+
+
+}
+
+void configure_DAC(void) {
+    char running = 1;
+    char compatible = 1;
+    int choice = 0;
+    while(running) {
+        system(CLEARSTR);
+        printf("\t\t** DAC Configuration Menu **\n");
+        printf("\t\t============================\n\n");
+        printf("1)\tSet Amplitude (Set value: %.2f V)\n",ampl_val);
+        printf("2)\tSet Mean Value (Set value: %.2f V)\n",mean_val);
+        printf("3)\tSet Frequency (Set value: %.2f Hz)\n",freq_val);
+        printf("4)\tSelect Output DAC (Set output DAC: DAC%d)\n",out_ADC);
+        printf("5)\tSave configuration in config file\n\n");
+        printf("6)\tReturn to main menu\n");
+        compatible = check_mean_amp_val(ampl_val, mean_val);
+        choice = choice_checker(1,6);
+
+        switch(choice) {
+            case 1: ampl_val = config_amplitude();
+                    break;
+            case 2: mean_val = config_mean();
+                    break;
+            case 3: freq_val = config_freq();
+                    break;
+            case 4: system(CLEARSTR);
+                    printf("Select output DAC (0-1): \n");
+                    out_ADC = choice_checker(0,1);
+                    break;
+            case 5: generate_config_file();
+                    break;
+            case 6: if(compatible) running = 0;
+                    else check_mean_amp_val(ampl_val, mean_val);
+                    break;
+
+        }
+    }
+}
+
+void gen_square(void) {
+    printf("Square");
+
+}
+
+void gen_sine(void) {
+    printf("Sine");
+
+}
+
+void gen_triangle(void) {
+    printf("Triangle");
+
+}
+
+void load_config(void) {
+    char file_name_buff[60];
+    char *file_name;
+    char *path_name = "config_files/";
+    char * file_path;
+    FILE *fp;
+    char str[60];
+    char dummy;
+
+    system(CLEARSTR);
+    printf("\t\t** Configuration file loader **\n");
+    printf("\t\t===============================\n\n");
+    printf("Saved config files:\n");
+    //"dir config_files /b /a-d" for WIN/DOS
+    //"ls -1" for UNIX
+    system(DIRLIST);
+    printf("\nType filename of desired config file: ");
+    fflush(stdout);
+    fgets (file_name_buff, 60, stdin);
+    file_name = malloc(strlen(file_name_buff));
+    remove_space(file_name, file_name_buff);
+    strtok(file_name, "\n");
+
+    file_path = malloc(strlen(path_name)+strlen(file_name)+1);
+    file_path[0] = '\0';
+    strcat(file_path, path_name);
+    strcat(file_path, file_name);
+    printf("Selected file: %s\n",file_path);
+
+
+
+   /* opening file for reading */
+   fp = fopen(file_path , "r");
+   if(fp == NULL) {
+      perror("Error opening file");
+      printf("File not found, press enter to continue...\n");
+      dummy = getchar();
+   }
+   else {
+    fscanf(fp,"AMPL=%lf MEAN=%lf FREQ=%lf ADC=%d",&ampl_val, &mean_val, &freq_val, &out_ADC);
+    fclose(fp);
+
+    printf("\nConfiguration file loaded successfully:\n\n");
+    printf("Amplitude: \t%lf V\n",ampl_val);
+    printf("Mean value: \t%lf V\n", mean_val);
+    printf("Frequency: \t%lf Hz\n", freq_val);
+    printf("Output DAC: \tDAC%d\n", out_ADC);
+    printf("Press enter to continue...");
+	fflush(stdout);
+    dummy = getchar();
+	}
+
+
+}
+
+void load_values_from_arguments(int argc, char **argv) {
+	int i;
+	char dummy;
+	system(CLEARSTR);
+
+		for(i=1; i < argc; i+=2) {
+			if(strcmp(argv[i],"-a") == 0) {
+				if(sscanf(argv[i+1], "%lf", &ampl_val) != 1) printf("Invalid amplitude value!\n") ;
+				else {
+					if(ampl_val > AMPL_UPPER_BOUND || ampl_val < AMPL_LOWER_BOUND) {
+						printf("Amplitude value out of range! Default value loaded...\n");
+						ampl_val = 2.5;
+						}
+				}
+			}
+			else if(strcmp(argv[i],"-m") == 0) {
+				if(sscanf(argv[i+1], "%lf", &mean_val) != 1)  printf("Invalid mean value!\n");
+				else {
+					if(mean_val > MEAN_UPPER_BOUND || mean_val < MEAN_LOWER_BOUND) {
+						printf("Mean value out of range! Default value loaded...\n");
+						mean_val = 2.5;
+					}
+				}
+			}
+			else if(strcmp(argv[i],"-f") == 0) {
+				if(sscanf(argv[i+1], "%lf", &freq_val) != 1) printf("Invalid frequency value!\n");
+				else {
+					if(freq_val > FREQ_UPPER_BOUND || freq_val < FREQ_LOWER_BOUND) {
+						printf("Frequency value out of range! Default value loaded...\n");
+						freq_val = 10.0;
+					}
+				}
+			}
+			else if(strcmp(argv[i],"-DAC") == 0) {
+				if(sscanf(argv[i+1], "%d", &out_ADC) != 1) printf("Invalid DAC selection!\n") ;
+				else {
+					if(out_ADC < 0 || out_ADC > 1) {
+						printf("Invalid DAC selection! Default value loaded...\n");
+						out_ADC = 0;
+					}
+				}
+			}
+			else {
+				printf("Invalid argument(s) given!\n");
+
+			}
+		}
+	if(!check_mean_amp_val(ampl_val, mean_val)) {
+		printf("Default values loaded for amplitude and mean value...\n");
+		ampl_val = 2.5;
+		mean_val = 2.5;
+	}
+	printf("\nValues loaded:\n\n");
+    printf("Amplitude: \t%lf V\n",ampl_val);
+    printf("Mean value: \t%lf V\n", mean_val);
+    printf("Frequency: \t%lf Hz\n", freq_val);
+    printf("Output DAC: \tDAC%d\n", out_ADC);
+    printf("Press enter to continue...");
+	fflush(stdout);
+    dummy = getchar();
+}
+
+
+
+int main(int argc, char **argv) {
+	char running = 1;
+	initscr();
+	//timeout(-1);
+	cbreak();
+	noecho();
+	keypad( stdscr, TRUE);
+	move(0,0);
+	if(argc > 1) {
+		load_values_from_arguments(argc, argv);
+	}
+    while(running) {
+        int choice = main_menu();
+        switch(choice) {
+            case 1: configure_DAC();
+                    break;
+            case 2: gen_square();
+                    break;
+            case 3: gen_sine();
+                    break;
+            case 4: gen_triangle();
+                    break;
+            case 5: load_config();
+                    break;
+            case 6: load_help_menu();
+            		break;
+            case 7: running = 0;
+        }
+    }
+    endwin();
+    return 0;
+}
